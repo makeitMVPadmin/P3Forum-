5078f3ee2d5058ccc56b796ca1cd50c4026a5c5a
Anthony Shellman <atshellman@gmail.com>
2024-04-19 11:54:48 -0600

    Add props and replace some placeholders

--- src/components/AskedQuestion/AskedQuestion.jsx
+++ src/components/AskedQuestion/AskedQuestion.jsx
@@ -4,37 +4,38 @@ import downvote from '../../assets/downvote.svg'
 import share from '../../assets/share.svg'
 import upvote from '../../assets/upvote.svg'
 
-const AskedQuestion = () => {
+const AskedQuestion = ({ communityID, createdAt, downVotes, questionContent,
+  topic, upVotes, user }) => {
 
-  const testImage = "https://firebasestorage.googleapis.com/v0/b/communiti-630fc.appspot.com/o/profile_thumbnails%2F%F0%9F%A6%86%20icon%20_linkedin_.png?alt=media&token=1bbb48cc-eb25-454e-98ad-8ba8fcaf4654" 
+  const { fullName, profilePhoto } = user 
 
   return (
     <section className='asked-question'>
       <div className='asked-question__container'>
         <div className='asked-question__user-details'>
           <div className='asked-question__avatar-container'>
-            <img className='asked-question__avatar' src={testImage} alt="test" />
+            <img className='asked-question__avatar' src={profilePhoto} alt="test" />
           </div>
-          <h2 className='asked-question__text'>Test User</h2>
+          <h2 className='asked-question__text'>{fullName}</h2>
           <h2 className='asked-question__text'>•</h2>
           <h2 className='asked-question__text'>5min</h2>
         </div>
         <div className='asked-question__body'>
-          <h2 className='asked-question__topic'>What kind of strawberry seed is the best?</h2>
-          <h2 className='asked-question__content'>I am trying to grow strawberries in my backyard and was wondering what kind of strawberry seeds is the best? I am a newbie gardener and trying to find a seed that is easy for beginners.</h2>
+          <h2 className='asked-question__topic'>{topic}</h2>
+          <h2 className='asked-question__content'>{questionContent}</h2>
         </div>
         <div className='asked-question__actions'>
           <div className='asked-question__button-container'>
             <button className='asked-question__button'>
               <img className='asked-question__upvote' src={upvote} alt="test" />
             </button>
-            <p className='asked-question__value'>0</p>
+            <p className='asked-question__value'>{upVotes}</p>
           </div>
           <div className='asked-question__button-container'>
             <button className='asked-question__button'>
               <img className='asked-question__downvote' src={downvote} alt="test" />
             </button>
-            <p className='asked-question__value'>0</p>
+            <p className='asked-question__value'>{downVotes}</p>
           </div>
           <div className='asked-question__button-container'>
             <button className='asked-question__button'>
