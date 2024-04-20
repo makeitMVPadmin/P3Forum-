1cb7616b7cf3bc913255774fa45e0b842110104a
Anthony Shellman <atshellman@gmail.com>
2024-04-20 14:18:14 -0600

    Remove test code and update properties

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -1,19 +1,15 @@
-import { useEffect } from "react"
 import { useGlobalContext } from "../../context.jsx";
 import QuestionCard from "../../components/QuestionCard/QuestionCard";
 import './Questions.scss'
 
 const Questions = () => {
 
-  const { userList, questionsList, answersList,
-    getUserList, getQuestionsList  } = useGlobalContext();
+  const { questionsList } = useGlobalContext();
   
- const question = questionsList[1]
- const user = userList.find(user => user.userID === question.userID)
-    console.log(question)
- const questionTest = () => {
-   const { communityID, createdAt, downVotes, questionContent,
-          topic, upVotes, userID } = question
+
+ const questions = questionsList.map(question => {
+   const { id, communityID, createdAt, downVotes, questionContent,
+      topic, upVotes, userID } = question
 
    return (
      <QuestionCard
@@ -24,26 +20,7 @@ const Questions = () => {
        topic={topic}
        upVotes={upVotes}
        userID={userID}
-       user={user}
-       key={userID}
-     />
-   )
- }
- 
- const questions = questionsList.map(question => {
-   const { communityID, createdAt, downVotes, questionContent,
-      topic, upVotes, userID } = question
-   return (
-     <QuestionCard
-       communityID={communityID}
-       createdAt={createdAt}
-       downVotes={downVotes}
-       questionContent={questionContent}
-       topic={topic}
-       upVotes={upVotes}
-       userID={userID}
-       user={user}
-       key={userID}
+       key={id}
      />
    )
  })
