e127deb928f14ca839841201268cb6f416e0354e
Dinne Kopelevich <dinne22@hotmail.com>
2024-04-22 17:48:21 -0600

    Add HeaderComponent

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import { useGlobalContext } from "../../context"
+import communitiLogo from "../../assets/communiti.svg"
+import homeLogo from "../../assets/house.svg"
+import eventLogo from "../../assets/calendar.svg"
+import communitiesLogo from "../../assets/silhouette.svg"
+import coffeeChatLogo from "../../assets/people_chatting.svg"
+import forumLogo from "../../assets/chat.svg"
+import defaultUser from "../../assets/icons/defaultUser.svg"
+import downCarrot from "../../assets/down_carrot.svg"
+import "./Header.scss"
+
+const Header = () => {
+  const {
+    randomUser2,
+  } = useGlobalContext();
+
+  console.log(randomUser2?.profilePhoto)
+
+
+  return (
+    <div className="header">
+      <div className="header__navigation">
+        <div className="header__main-icon">
+          <img src={communitiLogo} className="header__main-image"/>
+        </div>
+        <div className="header__icons">
+          <img src={homeLogo} className="header__images"/>
+          <div className="header__icon-titles">Home</div>
+        </div>
+        <div className="header__icons">
+          <img src={communitiesLogo} className="header__images"/>
+          <div className="header__icon-titles">Communities</div>
+        </div>
+        <div className="header__icons">
+          <img src={eventLogo} className="header__images"/>
+          <div className="header__icon-titles">Events</div>
+        </div>
+        <div className="header__icons">
+          <img src={coffeeChatLogo} className="header__images"/>
+          <div className="header__icon-titles">Coffee Chat</div>
+        </div>
+        <div className="header__icons">
+          <img src={forumLogo} className="header__images"/>
+          <div className="header__icon-titles-forum">Forum</div>
+        </div>
+      </div>
+      <div className="header__user-container">
+        <img src={randomUser2?.profilePhoto ? randomUser2?.profilePhoto : defaultUser} alt="Profile Pic" className="header__user-pic"/>
+        <img src={downCarrot} className="header__down-carrot"/>
+      </div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/ForumHomePage/ForumHomePage.jsx
+++ src/pages/ForumHomePage/ForumHomePage.jsx
@@ -1,10 +1,12 @@
 import "./ForumHomePage.scss";
+import Header from "../../components/Header/Header"
 import Questions from "../../components/Questions/Questions"
 import AskQuestionsContainer from "../../components/AskQuestionsContainer/AskQuestionsContainer";
 
 const Home = () => {
   return (
     <div className="home">
+      <Header />
       <AskQuestionsContainer />
       <Questions />
     </div>
