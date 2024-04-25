174e34eea1680275919ebf675d4f3089e92938f6
Anthony Shellman <atshellman@gmail.com>
2024-04-24 19:38:00 -0600

    Import Communities component

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,12 @@
 import "./Sidebar.scss";
 import Topics from "../Topics/Topics";
+import Communities from "../Communities/Communities"
 
 const Sidebar = () => {
   return (
     <section className="sidebar">
       <Topics />
+      <Communities />
     </section>
   );
 };
