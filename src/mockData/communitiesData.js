9c8f2a82a1bb5d38b9314c517b70d5d5a423b76a
Anthony Shellman <atshellman@gmail.com>
2024-04-25 13:39:57 -0600

    Import images, add id property

--- src/mockData/communitiesData.js
+++ src/mockData/communitiesData.js
@@ -1,30 +1,46 @@
+import Mac from '../assets/mock/mac.svg'
+import Tux from '../assets/mock/tux.svg'
+import JS from '../assets/mock/javascript.svg'
+import Python from '../assets/mock/python.svg'
+import Project from '../assets/mock/project-manage.svg'
+import Ruby from '../assets/mock/ruby.svg'
+import UX from '../assets/mock/ux-design.svg'
+
+
 const communitiesData = [
   {
-    // communityImage: ,
+    id: "11111111111111111111",
+    communityImage: Mac,
     communityName: "Mac Enthusiasts",
   },
   {
-    // communityImage: ,
+    id: "22222222222222222222",
+    communityImage: Tux,
     communityName: "Club Linux",
   },
   {
-    // communityImage: ,
+    id: "33333333333333333333",
+    communityImage: JS,
     communityName: "JavaScript Fan Club",
   },
   {
-    // communityImage: ,
+    id: "44444444444444444444",
+    communityImage: Python,
     communityName: "Python is Cool",
   },
   {
-    // communityImage: ,
+    id: "55555555555555555555",
+    communityImage: Project,
     communityName: "Project Manager Manor",
   },
   {
-    // communityImage: ,
+    id: "66666666666666666666",
+    communityImage: Ruby,
     communityName: "The Rubyists",
   },
   {
-    // communityImage: ,
+    id: "77777777777777777777",
+    communityImage: UX,
     communityName: "UX Design Crew",
   },
 ]
