8427ab4a53700ad408fffb1776c06f56952ca477
Anthony Shellman <atshellman@gmail.com>
2024-04-25 13:40:59 -0600

    Import mock community data, map and create cards

--- src/components/Communities/Communities.jsx
+++ src/components/Communities/Communities.jsx
@@ -1,14 +1,25 @@
 import CommunityCard from '../CommunityCard/CommunityCard'
+import communitiesData from '../../mockData/communitiesData'
 import './Communities.scss'
 
 const Communities = () => {
 
-  // const commmunityList = 
+  const commmunityList = communitiesData.map(community => {
+    const { id, communityImage, communityName } = community
+    return (
+      <CommunityCard
+        image={communityImage}
+        name={communityName}
+        key={id}
+      />
+    )
+  })
 
 
   return (
     <section className='communities'>
       <h2 className='communities__title'>Communities</h2>
+      { commmunityList }
     </section>
   )
 }
