d8d8df32b2dd0934bf14203396528895dbbf0942
Anthony Shellman <atshellman@gmail.com>
2024-04-24 19:36:37 -0600

    Initial commit

---
+++ src/mockData/communitiesData.js
@@ -0,0 +1,32 @@
+const communitiesData = [
+  {
+    // communityImage: ,
+    communityName: "Mac Enthusiasts",
+  },
+  {
+    // communityImage: ,
+    communityName: "Club Linux",
+  },
+  {
+    // communityImage: ,
+    communityName: "JavaScript Fan Club",
+  },
+  {
+    // communityImage: ,
+    communityName: "Python is Cool",
+  },
+  {
+    // communityImage: ,
+    communityName: "Project Manager Manor",
+  },
+  {
+    // communityImage: ,
+    communityName: "The Rubyists",
+  },
+  {
+    // communityImage: ,
+    communityName: "UX Design Crew",
+  },
+]
+
+export default communitiesData
