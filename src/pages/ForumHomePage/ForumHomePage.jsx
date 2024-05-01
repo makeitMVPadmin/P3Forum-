7cb3e9e6322bb985c9ea72cf2a0bf89bc63fe1af
drv0228 <110354201+drv0228@users.noreply.github.com>
2024-05-01 14:57:07 -0400

    coments removed

--- src/pages/ForumHomePage/ForumHomePage.jsx
+++ src/pages/ForumHomePage/ForumHomePage.jsx
@@ -1,17 +1,13 @@
 import "./ForumHomePage.scss";
-import Questions from "../../components/Questions/Questions"
+import Questions from "../../components/Questions/Questions";
 import AskQuestionsContainer from "../../components/AskQuestionsContainer/AskQuestionsContainer";
 import Sidebar from "../../components/Sidebar/Sidebar";
 
-import PostModal from "../../components/PostModal/PostModal";
-
 const Home = () => {
-
   return (
     <div className="home">
       <Sidebar />
       <div className="home-body">
-        <PostModal />
         <AskQuestionsContainer />
         <Questions />
       </div>
