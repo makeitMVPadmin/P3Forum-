0e100248ed270573418590329dc3df2262232a70
Dinne Kopelevich <dinne22@hotmail.com>
2024-04-30 18:11:28 -0600

    Add responsiveness to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,7 @@ import coffeeChatLogo from "../../assets/people_chatting.svg"
 import forumLogo from "../../assets/chat.svg"
 import defaultUser from "../../assets/icons/defaultUser.svg"
 import downCarrot from "../../assets/down_carrot.svg"
+import burgerBarLogo from "../../assets/burger-menu.svg"
 import "./Header.scss"
 
 const Header = () => {
@@ -18,32 +19,37 @@ const Header = () => {
     <div className="header">
       <div className="header__navigation">
         <div className="header__main-icon">
-          <img src={communitiLogo} className="header__main-image"/>
+          <img src={communitiLogo} alt="Communiti" className="header__main-image"/>
         </div>
-        <div className="header__icons">
-          <img src={homeLogo} className="header__images"/>
+
+        <div className="header__icons header__icons--mobile-hide">
+          <img src={homeLogo} alt="Home" className="header__images"/>
           <div className="header__icon-titles">Home</div>
         </div>
-        <div className="header__icons">
-          <img src={communitiesLogo} className="header__images"/>
+        <div className="header__icons header__icons--mobile-hide">
+          <img src={communitiesLogo} alt="Communities" className="header__images"/>
           <div className="header__icon-titles">Communities</div>
         </div>
-        <div className="header__icons">
-          <img src={eventLogo} className="header__images"/>
+        <div className="header__icons header__icons--mobile-hide">
+          <img src={eventLogo} alt="Events" className="header__images"/>
           <div className="header__icon-titles">Events</div>
         </div>
-        <div className="header__icons">
-          <img src={coffeeChatLogo} className="header__images"/>
+        <div className="header__icons header__icons--mobile-hide">
+          <img src={coffeeChatLogo} alt="Chat" className="header__images"/>
           <div className="header__icon-titles">Coffee Chat</div>
         </div>
-        <div className="header__icons">
-          <img src={forumLogo} className="header__images"/>
+        <div className="header__icons header__icons--mobile-hide">
+          <img src={forumLogo} alt="Forum" className="header__images"/>
           <div className="header__icon-titles-forum">Forum</div>
         </div>
       </div>
-      <div className="header__user-container">
+      <div className="header__user-container header__icons--mobile-hide">
         <img src={randomUser2?.profilePhoto ? randomUser2?.profilePhoto : defaultUser} alt="Profile Pic" className="header__user-pic"/>
-        <img src={downCarrot} className="header__down-carrot"/>
+        <img src={downCarrot} alt="Down Arrow" className="header__down-carrot"/>
+      </div>
+
+      <div className="header__icons header__icons--mobile-show">
+        <img src={burgerBarLogo} alt="Menu" className="header__images"/>
       </div>
     </div>
   )
