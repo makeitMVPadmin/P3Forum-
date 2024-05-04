1ea61a20b79c4ba9b73b03a99edc1805cdd2b74a
Dinne Kopelevich <dinne22@hotmail.com>
2024-05-03 18:54:08 -0600

    Style Topics and Comm to match

--- src/components/TopicCard/TopicCard.js
+++ src/components/TopicCard/TopicCard.js
@@ -1,20 +1,20 @@
-import "./TopicCard.scss";
-import start from "../../assets/icons/Favourite Commuinity.svg";
+// import "./TopicCard.scss";
+// import start from "../../assets/icons/Favourite Commuinity.svg";
 
-const TopicCard = ({ topic }) => {
-  return (
-    <section className="topics-section">
-      <div className="topics-section__box">
-        <div className="topics-section__holder"> </div>
-        <p className="topics-section__title">{topic}</p>
-      </div>
-      <img
-        className="topics-section__fav"
-        src={start}
-        alt="start icon represent how users like this topic"
-      />
-    </section>
-  );
-};
+// const TopicCard = ({ topic }) => {
+//   return (
+//     <section className="topics-section">
+//       <div className="topics-section__box">
+//         <div className="topics-section__holder"> </div>
+//         <p className="topics-section__title">{topic}</p>
+//       </div>
+//       <img
+//         className="topics-section__fav"
+//         src={start}
+//         alt="start icon represent how users like this topic"
+//       />
+//     </section>
+//   );
+// };
 
-export default TopicCard;
+// export default TopicCard;
